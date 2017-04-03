f98c8338eb210e6d423e671113fa22539e4189bd
Tim Schafer <timschafer12@gmail.com>
2017-04-03 13:45:59 -0400

    Table test

--- PDF Generation Test/MainClass.cs
+++ PDF Generation Test/MainClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
@@ -12,8 +13,39 @@ namespace PDF_Generation_Test
     {
         public static void Main(string[] args)
         {
+            string filePath = @"C:\Users\Tim\Desktop\";
+            string fileName = "Test.pdf";
 
+            List<IElement> elementsList = new List<IElement>();
+            int numOfCols = 3;
+            int numOfRows = 3;
+            PdfPTable table = new PdfPTable(numOfCols);
+            PdfPCell header = new PdfPCell(new Phrase("Table header"));
+            header.Colspan = 3;
+            header.HorizontalAlignment = 1;
+            table.AddCell(header);
+            for (int i = 1; i <= numOfRows; i++)
+            {
+                for (int j = 1; j <= numOfRows; j++)
+                {
+                    table.AddCell("Row " + i + " Col " + j);
+                }
+            }
+            elementsList.Add(table);
+            IElement[] elements = elementsList.ToArray();
+
+            generateDocument(filePath+fileName, elements);
         }
-        public void generateDocument(string filePath, )
+        public static void generateDocument(string filePath, IElement[] elements)
+        {
+            Document doc = new Document();
+            PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+            doc.Open();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                doc.Add(elements[i]);
+            }
+            doc.Close();
+        }
     }
 }
