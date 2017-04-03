9e4c96a462662c6bf2326703e4a726eeac6d3f99
Tim Schafer <timschafer12@gmail.com>
2017-04-03 12:16:58 -0400

    Version 1

---
+++ PDF Generation Test/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDF_Generation_Test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public void generateDocument(string filePath, )
+    }
+}
---
+++ PDF Generation Test/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDF_Generation_Test
+{
+    class Report
+    {
+
+    }
+}
